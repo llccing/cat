a045144f2ce7e5a0cfdb4c8d29a29e33ded71930
刘春峰 <liuchunfeng@btzh.cn>
2017-08-14 18:41:27 +0800

    '机器人'

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,27 +2,22 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target  ="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <group>
+      <x-input title="内容：" v-model="value"></x-input>
+    </group>
+
+    <x-button type="primary">发送内容</x-button>
   </div>
 </template>
 
 <script>
+import { XInput, XButton } from 'vux'
+
 export default {
-  name: 'hello',
+  components: {
+    XInput,
+    XButton
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
